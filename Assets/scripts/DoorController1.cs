c3f692a2e3ea8bc072d75d8c5aba18753b8e29a2
009GetRandom <37257709+009GetRandom@users.noreply.github.com>
2020-09-24 09:43:53 +0930

    Added things from unity

---
+++ Assets/scripts/DoorController1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using TMPro;
+using UnityEngine;
+
+
+public class DoorController1 : MonoBehaviour
+{
+    public TextMeshProUGUI CountText;
+    public GameObject winTextObject;
+    public GameObject dooropen;
+    private void Start()
+    {
+        dooropen.SetActive(false);
+    }
+    private void OnTriggerStay(Collider other)
+   {
+        dooropen.SetActive(true);
+
+        if (other.tag == "Door")     
+        {
+           Animator anim = other.GetComponentInChildren<Animator>();
+           if (Input.GetKeyDown(KeyCode.E))
+            anim.SetTrigger("OpenClose");
+        }
+   }
+}
